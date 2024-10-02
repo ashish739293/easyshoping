566f0388ae97095ad9b8a4913cb775579e784d40
Nisha7060 <nisha971943@gmail.com>
2024-10-02 17:48:09 +0530

    debug true Signed-off-by: Nisha7060 <nisha971943@gmail.com>

--- ossproject/settings.py
+++ ossproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a&s5cnog7m4)syanijf#%aabe8j33wt1r^p1^qp6%(%!ls)8m!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # Set your allowed hosts properly
 
